557bcdbb7502c98d9fa797d097d7a3a77acd988c
Stefan Gouldson <stefangouldson@gmail.com>
2022-03-23 15:18:18 +0000

    mongooooooooose

---
+++ app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const res = require('express/lib/response');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('623b2c784273894b2077f135')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    `mongodb+srv://user:password@node-cluster.zlnee.mongodb.net/db?retryWrites=true&w=majority`
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: 'stefan',
+          email: 'stefangouldson@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
